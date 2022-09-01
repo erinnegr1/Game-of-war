b1f1b3e76b6b184171efb2745fd6db676324acf5
Erinne Grant <erinne.grant@gmail.com>
2022-08-31 19:34:30 -0700

    new game mechanics

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 import Deck from './deck.js'
 
 const computerCardSlot = document.querySelector(".computer-card-slot") //reference to computer-card-slot card-slot in HTML class
+const playerCardSlot = document.querySelector(".player-card-slot")
+const computerDeckElement = document.querySelector('.computer-deck')
+const playerDeckElement = document.querySelector('.player-deck')
+const text = document.querySelector('text')
 
-startGame() {  //gets called right away
-function startGame()
-  const deck = new Deck()
-  deck.shuffle()
 
-  const deckMidpoint = Math.ceil(deck.numberOfcards/2) // the reason we have math.ciel is we want to divide an odd number (ie 51 cards. 26 would go to one deck, and 25 to another)
+
+let playerDeck, computerDeck  //variables to be accessed anywhere
+
+startGame()   //gets called right away
+function startGame() {
+ const deck = new Deck()
+ deck.shuffle()
+
+  const deckMidpoint = Math.ceil(deck.cards.length / 2)// the reason we have math.ciel is we want to divide an odd number (ie 51 cards. 26 would go to one deck, and 25 to another)
+  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint)) // first 26 cards
+  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.cards.length)) // last 26 cards - remember the variable to cover .length here
+  
+  cleanBeforeRound()  // this clears up the page before every round and calls to the written function below
 }
 
-computerCardSlot.appendChild(deck.cards[0].getHTML()) //this grabs the first card in our deck, the color of the suit, and appends it to that div class area
+
+function cleanBeforeRound() {
+   computerCardSlot.innerHTML =''
+   playerCardSlot.innerHTML =''
+   text.innerText = ''
+
+   updateDeckCount()
+}
+
+function updateDeckCount(){
+    computerDeckElement.innter
+}
